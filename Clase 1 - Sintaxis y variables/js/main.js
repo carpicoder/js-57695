a2e4d67b32a7a30a5eba5ab4cbd5f1f627156576
carpicoder <webcarpi@gmail.com>
2024-04-18 17:02:46 +0200

    Clase 1

---
+++ Clase 1 - Sintaxis y variables/js/main.js
@@ -0,0 +1,83 @@
+// Comentarios de una sola línea
+
+/* 
+    Comentarios de
+    más de una
+    línea
+*/
+
+//! extensión BETTER COMMENTS
+//! rojo
+//? azul
+//todo naranja
+//* verde clarito
+// normal
+
+/* 
+String
+
+"comillas dobles"
+'comillas simples'
+`backticks`
+
+*/
+
+// let apodo; // undefined
+// const apellido = "Coletta"; // String / cadena de texto
+// const anioNacimiento = 1995; // Number / número
+
+
+//? Asignación
+
+// // declarar
+// let nombre;
+// let apellido;
+// let edad;
+
+// // asignar
+// nombre = "Matias";
+// apellido = "Coletta";
+// edad = 28;
+
+// // declarar y asignar al mismo tiempo
+// let nombre = "Matias";
+// const apellido = "Coletta";
+// let edad = 28;
+
+// edad = 29;
+// apellido = "Coder";
+
+//? operaciones básicas
+
+// let numero1 = 4;
+// let numero2 = 2;
+
+// let resultadoSuma = numero1 + numero2;
+// let resultadoResta = numero1 - numero2;
+// let resultadoMultiplicacion = numero1 * numero2;
+// let resultadoDivision = numero1 / numero2;
+
+//? prompt
+
+// let nombreIngresado = prompt("Ingresá tu nombre:");
+
+//? console.log
+
+// console.log("Hola, mundo!");
+
+// let nombre = "Carpi";
+// console.log(nombre);
+
+// let numero1 = 4;
+// let numero2 = 2;
+// let resultadoSuma = numero1 + numero2;
+
+// console.log(resultadoSuma);
+
+let saludo = "Hola";
+let nombre = prompt("Ingresá tu nombre");
+let saludoCompleto = saludo + " " + nombre;
+
+console.log(saludoCompleto);
+
+// alert(saludoCompleto);
